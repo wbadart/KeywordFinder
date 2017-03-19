8ad1e4c9313de40ad7d720c2034eadb71c0bc0f7
Will Badart <wbadart@nd.edu>
2017-03-18 21:54:22 -0400

    Added usage, config file option Reads 1st command line argument as config file name. Defaults to "./config" if argument not given.

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,27 @@
  * created: MAR 2017
  */
 
+#include <cstdlib>      // exit
 #include <iostream>
+#include <string>
 #include "config.hpp"
+
+void usage(int status=0);
 
 int main(int argc, char *argv[]){
 
-    Config config;
+    std::string config_fname(argc == 2 ? argv[1] : "./config");
+    Config config(config_fname);
+    usage();
 
     return 0;
+}
+
+void usage(int status){
+    std::cerr << "usage: site-tester CONFIG_FILENAME" << std::endl
+              << "Options:" << std::endl
+              << "\tCONFIG_FILENAME    Specify name of config file "
+              << "(default: \"./config\")" << std::endl;
+    exit(status);
 }
 
